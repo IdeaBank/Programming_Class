c357fd1d2f751f1d436c962a0ee1a2e3b3a474e5
Woojin Lim <isp_lwj@naver.com>
2018-03-25 20:24:04 +0900

    Applied some changes

--- Application Programming/0001. Basic_Print_Sentences/Information/src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-
-public class Test{
-    public static void main(String[] args) {
-        Human Woojin = new Human("임우진", 18, "남", 170, 57, "한국디지털미디어고등학교","해킹방어과","20010911");
-        //Woojin.printInfo_printf();
-        Woojin.printInfo_println();
-    }
-}
